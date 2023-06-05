f81abbc9bebf4a958d76d5084e566fb7b5a04dee
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-06-05 23:40:41 +0200

    Using constants instead of direct paths.
    In this commit, we replaced the use of direct paths with the use of constants that are stored in another file
    
    this will make the code more versatile and optimized.

--- src/services/http-service/products.service.ts
+++ src/services/http-service/products.service.ts
@@ -1,16 +1,17 @@
 import { HttpService } from "./implementation/http.service";
+import { ROUTER_KEYS } from "../../modules/common/consts/app-keys.const";
 
 export class ProductService extends HttpService {
   async getProducts(category: string, search: string) {
-    return await this.get(`products/all?category=${category}&search=${search}`);
+    return await this.get(`${ROUTER_KEYS.PRODUCTS}/all?category=${category}&search=${search}`);
   }
 
   async getProductById(id: string) {
-    return await this.get(`products/${id}`);
+    return await this.get(`${ROUTER_KEYS.PRODUCTS}/${id}`);
   }
 
   async getCategories() {
-    return await this.get("products/categories");
+    return await this.get(`${ROUTER_KEYS.PRODUCTS}/categories`);
   }
 }
 
